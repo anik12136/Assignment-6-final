77b05e89e19ebd8bf557b0753e1dc0cf1fca2465
Mac <mac@Macs-MacBook-Air.local>
2023-03-06 00:59:40 +0600

    semi ok

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@ const displayTools = (tools, dataLimit) => {
         toolDiv.classList.add('col');
         toolDiv.innerHTML = `
         <div class="card">
-            <img src="${tool.image}"class="card-img-top" alt="...">
+            <img src="${tool.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title fw-bold">Features</h5>
                 <p class="card-text">1. ${tool.features[0]}</p>
@@ -44,7 +44,7 @@ const displayTools = (tools, dataLimit) => {
 
                 </div>
                 <div>
-                <button onclick="loadToolDetails('${tool.id}')" type="button" class="fa-solid fa-arrow-right bg-danger bg-opacity-25 p-3 rounded-circle border-0 text-danger" data-bs-toggle="modal" data-bs-target="#toolDetailsModal"></button>  
+                <button onclick="loadToolDetails('${tool.id}')" type="button" class=" fa-solid fa-arrow-right bg-danger bg-opacity-25 p-3 rounded-circle border-0 text-danger" data-bs-toggle="modal" data-bs-target="#toolDetailsModal"></button>  
                 </div>
                 
             </div>
@@ -80,11 +80,53 @@ const loadToolDetails = async id => {
 }
 
 const displayToolDetails = tool => {
-    console.log(tool);
     const description = document.getElementById('description');
     description.innerText = tool.description;
     const modImage = document.getElementById('modalImage');
-    modImage.innerHTML = `<img src="${tool.image_link[0]}" class="card-img-top" alt="...">`
+    modImage.innerHTML = `<img src="${tool.image_link[0]}" style="height:250px;" class="card-img-top" alt="...">`
+    const modalFeature = document.getElementById('modalFeature');
+    // console.log(tool.features[1].feature_name)
+    modalFeature.innerHTML = `
+        <p> . ${tool.features[1].feature_name}</p>
+        <p> . ${tool.features[2].feature_name}</p>
+        <p> . ${tool.features[3].feature_name}</p>
+    `
+    const modalIntegrations = document.getElementById('modalIntegrations');
+    modalIntegrations.innerHTML = `
+        <p> . ${tool ? tool.integrations[0] :'' }</p>
+        <p> . ${tool ? tool.integrations[1] :''}</p>
+        <p> . ${tool ? tool.integrations[2] :''}</p>    
+    `
+    console.log(tool)
+    const modalCard2Heading = document.getElementById('modal-card2-heading');
+    modalCard2Heading.innerHTML = `${tool.input_output_examples[0].input}`
+    const modalCard2Paragraph = document.getElementById('modal-card2-paragraph');
+    modalCard2Paragraph.innerHTML = `${tool.input_output_examples[0].output ? tool.input_output_examples[0].output :''}`
+    const pricing1 = document.getElementById('pricing1');
+    pricing1.innerHTML = `${tool.pricing[0].price}`
+   
+    const pricing2 = document.getElementById('pricing2');
+    pricing2.innerHTML = `${tool.pricing[1].price}`
+   
+    const pricing3 = document.getElementById('pricing3');
+    pricing3.innerHTML = `${tool.pricing[2].price}`
+
+      const accuracyContainer = document.getElementById('accuracyContainer');
+      
+       if(tool.accuracy.score!== null){
+        accuracyContainer.innerHTML =`<p style="margin-top: -245px; margin-left: 300px; margin-right: 10px; font-size:12px" class="bg-danger px-3 py-1 rounded-3 ">
+       ${tool.accuracy.score }% accuracy</p>`
+       }
+       else{
+        accuracyContainer.innerHTML =`''`
+       }
+      
+
+
+    //   accuracyContainer.innerHTML =`<p style="margin-top: -245px; margin-left: 300px; margin-right: 10px;" class="bg-danger px-3 py-1 rounded-3">
+    //                     <span id="accuracy"> ${tool.accuracy.score }</span> accuracy</p>`
+   
+    
     // const toolDetails = document.getElementById('tool-details');
     // console.log(tool.mainFeatures.sensors[0]);
     // toolDetails.innerHTML = `
@@ -94,7 +136,5 @@ const displayToolDetails = tool => {
     //     <p>Sensor: ${phone.mainFeatures.sensors ? phone.mainFeatures.sensors[0] : 'no sensor'}</p>
     // `
 }
-
-
 
 loadTools(6);
